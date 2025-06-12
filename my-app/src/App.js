6ceda840502047ce8e6ae0195a93ed7cc754a0c1
patriarchpam <patriarchpam700@gmail.com>
2025-06-12 17:03:09 +0100

    my first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TodoList from './TodoList';
+import './App.css';
+
+
+function App() {
+    return(
+      <div className="app">
+        <h1>ToDo List</h1>
+        <TodoList/>
+      </div>
+    );
+}
+
+export default App;
